3bbbec79c157aea387a13d36e42377acb62d4633
Dakshi Khatri <dakshi01@gmail.com>
2022-06-06 16:07:01 +0530

    Initial Commit

---
+++ 37Signals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  37Signals
+//
+//  Created by Dakshi Khatri on 06/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 37Signals/_7SignalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7SignalsApp.swift
+//  37Signals
+//
+//  Created by Dakshi Khatri on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct _7SignalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
